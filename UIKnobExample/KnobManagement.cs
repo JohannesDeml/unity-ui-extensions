3e7049dfc36383e7e0e4c231c3a84ea50ead98c7
Simon (darkside) Jackson <darkside@zenithmoon.com>
2020-10-21 18:11:17 +0100

    Added UIKnob example

---
+++ UIKnobExample/KnobManagement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UI.Extensions;
+
+public class KnobManagement : MonoBehaviour
+{
+    public Text KnobValue;
+    public InputField SetKnobValue;
+    public UI_Knob Knob;
+    
+    // Start is called before the first frame update
+    public void UpdateKnobValue()
+    {
+        Knob.SetKnobValue(float.Parse(SetKnobValue.text));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        KnobValue.text = Knob.knobValue.ToString();
+    }
+}
